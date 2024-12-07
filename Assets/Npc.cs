cbeda7eaaab7b396fd3e2ff51e5b1d92e30d6f22
Sinpolm <130205740+Sinpolm@users.noreply.github.com>
2024-12-07 10:31:17 -0500

    jus

---
+++ Assets/Npc.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[System.Serializable]
+
+
+public class Npc : MonoBehaviour {
+    public string npcName; // Name of the NPC
+    public string npcText; // Text to display when interacting with the NPC
+}
